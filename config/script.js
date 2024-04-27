579c49746d7a20299d804fa588fba1cdc0596299
Kenn <141384556+Hirakushiii@users.noreply.github.com>
2024-04-27 21:00:03 +0700

    first commit

---
+++ config/script.js
@@ -0,0 +1,10 @@
+document.querySelector("#dark-mode").addEventListener("click", () => {
+    const e = document.querySelector("#dark-mode");
+    e.classList.remove('bi bi-moon fs-5');
+    e.classList.add('bi bi-sun-fill fs-5');
+});
+
+function changedarkmode() {
+    e.classList.remove("bi bi-sun-fill fs-5"); // Remove the original class
+    e.classList.add("bi bi-moon fs-5"); // Add the new class
+}
