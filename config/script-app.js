07bfbd60495b044dc28f34bbdef9f07e2b0d0f2c
Kenn <141384556+Hirakushiii@users.noreply.github.com>
2024-05-06 17:50:22 +0700

    ADD APP MAIN SECTION

---
+++ config/script-app.js
@@ -0,0 +1,22 @@
+const urlParams = new URLSearchParams(window.location.search);
+
+// Access a specific parameter:
+const name = urlParams.get('n');
+const today = new Date();
+
+const hours = today.getHours();
+const sayhello = sayhelloo();
+function sayhelloo(){
+    if(hours <= 11){
+        return 'Pagi';
+    }else if(hours >= 11 && hours < 15){
+        return 'Siang';
+    }else if(hours >=15 && hours <= 19 ){
+        return 'Sore'
+    }else{
+        return 'Malam';
+    }
+}
+
+
+document.querySelector('#sayhello').innerHTML = `Selamat ${sayhello} ${name}`;
--- config/script.js
+++ config/script.js
@@ -1,10 +1,12 @@
-document.querySelector("#dark-mode").addEventListener("click", () => {
-    const e = document.querySelector("#dark-mode");
-    e.classList.remove("bi bi-moon fs-5");
-    e.classList.add("bi bi-sun-fill fs-5");
-});
+const btn_login = document.querySelectorAll('.gotoapp');
 
-function changedarkmode() {
-  e.classList.remove("bi bi-sun-fill fs-5"); // Remove the original class
-  e.classList.add("bi bi-moon fs-5"); // Add the new class
-}
+btn_login.forEach(e => {
+    e.addEventListener('click', ()=>{
+        let confirm = prompt('Masukkan namamu :');
+        if(confirm.length === 0){
+            return alert('Namamu kosong wak!');
+        }else{
+            document.location.href = `app/index.html?n=${confirm}`;
+        }
+    })
+});
